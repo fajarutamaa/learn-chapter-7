4ef6d500f73188a197d4dbddd102c918981e380f
fajarutamaa <fajardwiutomo75@gmail.com>
2023-11-21 21:52:28 +0700

    update sentry configuration

--- src/libs/sentry.js
+++ src/libs/sentry.js
@@ -1,14 +1,15 @@
 const Sentry = require('@sentry/node')
-const ProfilingIntegration = require('@sentry/profiling-node')
+// const ProfilingIntegration = require('@sentry/profiling-node')
+
+// const app = express()
 
 function initSentry(dsn) {
     Sentry.init({
         dsn,
-        integrations: [
-            new Sentry.Integrations.Http({ tracing: true }),
-            new Sentry.Integrations.Express({ app: express() }),
-            new ProfilingIntegration(),
-        ],
+        //  integrations: [
+        
+        //      new ProfilingIntegration(),
+        //  ],
         tracesSampleRate: 1.0,
         profilesSampleRate: 1.0,
     })
