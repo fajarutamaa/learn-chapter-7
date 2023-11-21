6a132fd49e02d504268261603903b0f2f110a0f3
fajarutamaa <fajardwiutomo75@gmail.com>
2023-11-21 22:10:57 +0700

    add setup sentry.io

--- index.js
+++ index.js
@@ -11,6 +11,23 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use('/api/v1/', router)
 
+app.use(Sentry.Handlers.requestHandler());
+
+// TracingHandler creates a trace for every incoming request
+app.use(Sentry.Handlers.tracingHandler());
+
+
+app.use(Sentry.Handlers.errorHandler());
+
+// Optional fallthrough error handler
+app.use(function onError(err, req, res, next) {
+  // The error id is attached to `res.sentry` to be returned
+  // and optionally displayed to the user for support.
+  res.statusCode = 500;
+  res.end(res.sentry + "\n");
+});
+
+
 app.get('/', (req, res) => {
     try {
         console.log('Hello World')
