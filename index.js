6d1a4ee696d574074f0ad3590e6fc52d43757452
fajarutamaa <fajardwiutomo75@gmail.com>
2023-11-22 22:15:10 +0700

    update sentry.io

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const router = require('./src/routes/route')
-const { initSentry } = require('./src/libs/sentry')
+const { iinitializeSentry } = require('./src/libs/sentry')
 require('dotenv').config()
 
 const port = process.env.PORT || 3000
@@ -11,32 +11,11 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use('/api/v1/', router)
 
-app.use(Sentry.Handlers.requestHandler());
 
-// TracingHandler creates a trace for every incoming request
-app.use(Sentry.Handlers.tracingHandler());
-
-
-app.use(Sentry.Handlers.errorHandler());
-
-// Optional fallthrough error handler
-app.use(function onError(err, req, res, next) {
-  // The error id is attached to `res.sentry` to be returned
-  // and optionally displayed to the user for support.
-  res.statusCode = 500;
-  res.end(res.sentry + "\n");
+Sentry.init({
+    dsn: process.env.SENTRY_DSN,
+    tracesSampleRate: 1.0,
 });
-
-
-app.get('/', (req, res) => {
-    try {
-        console.log('Hello World')
-    } catch (error) {
-        Sentry.captureException(error);
-    }
-})
-
-initSentry(process.env.SENTRY_DSN)
 
 app.listen(port, () => {
     console.log(`Server is running on port : ${port}`)
--- src/libs/sentry.js
+++ src/libs/sentry.js
@@ -1,18 +1,13 @@
-const Sentry = require('@sentry/node')
-// const ProfilingIntegration = require('@sentry/profiling-node')
+const Sentry = require("@sentry/node");
 
-// const app = express()
-
-function initSentry(dsn) {
+function initializeSentry(dsn) {
     Sentry.init({
         dsn,
-        //  integrations: [
-        
-        //      new ProfilingIntegration(),
-        //  ],
         tracesSampleRate: 1.0,
-        profilesSampleRate: 1.0,
-    })
+    });
 }
 
-module.exports = { initSentry }
+
+module.export = {
+    initializeSentry
+}
